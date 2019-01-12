105301897b015499142b8125e5403b4ca436a98d
Djordje <djole.tosic96@gmail.com>
2019-01-12 19:13:01 +0100

    Izmenjene brzine, kacenje na debla, sneg i kesa

--- project/Config.py
+++ project/Config.py
@@ -6,7 +6,7 @@ class Config:
     woodId = -1
     turtleId = -1
 
-    attachedId = -1
+    speedFrog = 50
 
     speedcar = 8
     speedfireman = 5
@@ -18,5 +18,15 @@ class Config:
 
     speedturtle = 8
 
-    mode = ["normal", "snow", "rain"]
+    attachSpeedWood = 10
+    attachSpeedTurtle = 8
+
+    mode = ["normal", "snow", "rain", "normal", "normal"]
     vreme = ""
+    attachedId = -1
+
+    flowerOne = True
+    flowerTwo = True
+    flowerThree = True
+    flowerFour = True
+    flowerFive = True
